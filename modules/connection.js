4257c8e1abb790b675182f401079b07113df5708
ziyadparekh <ziyad@poutsch.com>
2013-11-14 19:23:38 -0500

    db setup

--- modules/connection.js
+++ modules/connection.js
@@ -1,9 +1,9 @@
 exports.mysql = function(){
         var mysql = require('mysql');
         var connection = mysql.createConnection({
-                host     : "localhost",
-                user     : "root",
-                password : "grindhouse",
+                host     : "blaze.c3yptlhkvjtk.us-west-2.rds.amazonaws.com",
+                user     : "blaze",
+                password : "ziyadfm1",
                 port     : 3306,
                 database : "helix"
         });
